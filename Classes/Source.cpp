9e15728b279b070adc1d6fb3164481f68c507e07
Michael Lafosse <s188037@students.aie.edu>
2018-09-17 17:56:47 -0500

    added classes

---
+++ Classes/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class Stack
+{
+private:
+	int values[20];
+	int count = 0;
+
+public:
+	bool isEmpty()
+	{
+
+	}
+
+	int top()
+	{
+
+	}
+	void pop()
+	{
+		
+	}
+	void push(int value)
+	{
+		count++;
+	}
+
+};
+
+main()
+{
+	Stack stack;
+	stack.push(20);
+}
