c7ce59558937c327c2369b916ce1ea19c4147e4f
Michael Lafosse <s188037@students.aie.edu>
2018-09-18 17:35:15 -0500

    almost done with hw

--- Classes/Source.cpp
+++ Classes/Source.cpp
@@ -7,28 +7,63 @@ private:
 	int count = 0;
 
 public:
-	bool isEmpty()
-	{
-
-	}
-
 	int top()
 	{
-
+		std::cout << values[count - 1] << std::endl;
+		return values[count];
 	}
 	void pop()
 	{
-		
+		values[count - 1] = -1;
+		count--;
 	}
 	void push(int value)
 	{
-		count++;
+		if (value <= 9 && value >= 0)
+		{
+			if (count < 19)
+			{
+				values[count] = value;
+				count++;
+			}
+		}
+		if (value == 43)
+		{
+			values[count - 2] += values[count - 1];
+			pop();
+			std::cout << "worked";
+		}
+		if (value == 45)
+		{
+			values[count -1] - values[count - 2];
+			pop();
+		}
 	}
-
 };
 
-main()
+int main()
 {
+	int input;
+	bool keepPlaying = true;
 	Stack stack;
-	stack.push(20);
+	while (keepPlaying)
+	{
+		std::cin >> input;		
+		if(std::cin.fail())
+			do
+			{ 
+				std::cin.clear();
+				std::cin.ignore();
+				char op;
+				std::cin >> op;
+				input = op;
+			} while (std::cin.fail());
+		if (char(input) == 'q')
+		{
+			keepPlaying = false;
+			continue;
+		}
+		stack.push(input);
+	}
+	system("pause");
 }
