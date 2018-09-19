ae8be236ec631d7c39f9ff205099fa6743578eed
MichaelLafosse99 <michaellafosse99@gmail.com>
2018-09-18 21:26:25 -0500

    Finished Calc Simulator

--- Classes/Source.cpp
+++ Classes/Source.cpp
@@ -7,16 +7,23 @@ private:
 	int count = 0;
 
 public:
+
+	//Top value in the stack
 	int top()
 	{
 		std::cout << values[count - 1] << std::endl;
 		return values[count];
 	}
+
+	//function that changes the value, "pops", to -1.
 	void pop()
 	{
 		values[count - 1] = -1;
 		count--;
 	}
+	//function that pushes a value between 0-9 onto the stack,
+	//if count is less than 19 then count increments, basically
+	//pointing to the next index.
 	void push(int value)
 	{
 		if (value <= 9 && value >= 0)
@@ -27,38 +34,72 @@ public:
 				count++;
 			}
 		}
+		//if the value is 43, "+", the top of the stack and the index right before it,
+		//add up and print out the combined value
 		if (value == 43)
 		{
 			values[count - 2] += values[count - 1];
 			pop();
-			std::cout << "worked";
+			std::cout << "Result: " << std::endl;
+			std::cout << values[count - 1];
 		}
+		//if the value is 45, "-", the top of the stack and the index right before it,
+		//are subtracted and print out the result.
 		if (value == 45)
 		{
-			values[count -1] - values[count - 2];
+			values[count - 2] -= values[count - 1];
 			pop();
+			std::cout << "Result: " << std::endl;
+			std::cout << values[count - 1];
 		}
 	}
 };
+
+//Function that is supposed to convert char to int.
+void convert(char input)
+{
+	char op = input;
+	input = '+';
+
+	input = (int)op;
+	//std::cout << (int)temp;
+}
 
 int main()
 {
 	int input;
 	bool keepPlaying = true;
 	Stack stack;
+
+	std::cout << "How to use this program: " << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Input as many numbers as you want (preferably two numbers)." << std::endl;
+	std::cout << "After you have entered two numbers, input either a + or a - to add or subtract the two numbers." << std::endl;
+	std::cout << "Once inputting either + or - you have to input them again, mainly because this guy can't program.." << std::endl;
+	std::cout << "The same thing applies if you input 'q' or 'Q'.." << std::endl;
+	std::cout << "I don't make the instructions, I just state them..." << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Input: " << std::endl;
+
+	//The loop keeps going as long as the user doesn't input 'q' or 'Q',
+	//if 'q' or 'Q' is inputted, it sets the loop as false, ending the program.
 	while (keepPlaying)
 	{
-		std::cin >> input;		
-		if(std::cin.fail())
+		std::cin >> input;	
+
+		if (std::cin.fail())
 			do
-			{ 
+			{
 				std::cin.clear();
 				std::cin.ignore();
 				char op;
 				std::cin >> op;
 				input = op;
 			} while (std::cin.fail());
-		if (char(input) == 'q')
+
+		if (char(input) == 'q' || char(input) == 'Q')
 		{
 			keepPlaying = false;
 			continue;
